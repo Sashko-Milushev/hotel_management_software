46a998dd621e777a4e56d26f0649ee50544e8b53
Sashko Milushev <milushev.sashko@gmail.com>
2023-03-04 17:47:29 +0200

    Implement basic account views, urls and templates

--- hotel_management_software/accounts/urls.py
+++ hotel_management_software/accounts/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = (
+from django.urls import path
 
+from hotel_management_software.accounts.views import SignInView, ChangePasswordView, ChangePasswordDoneView
+
+urlpatterns = (
+    path('login/', SignInView.as_view(), name='login user'),
+    path('change-password-done/', ChangePasswordDoneView.as_view(), name='change password done'),
+    path('user/<int:pk>/password-change/', ChangePasswordView.as_view(), name='change password')
 )
--- hotel_management_software/accounts/views.py
+++ hotel_management_software/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth import views as auth_view, get_user_model
+from django.urls import reverse_lazy
+
+UserModel = get_user_model()
+
+
+class SignInView(auth_view.LoginView):
+    template_name = 'accounts/sign-in-page.html'
+
+
+class SignOutView(auth_view.LogoutView):
+    next_page = reverse_lazy('login user')
+
+
+class ChangePasswordView(auth_view.PasswordChangeView):
+    template_name = 'accounts/change-password-page.html'
+
+
+class ChangePasswordDoneView(auth_view.PasswordChangeDoneView):
+    template_name = 'accounts/change-password-done-page.html'
