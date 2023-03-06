ac86177e38c913f06d225526b3914beb347078c3
Sashko Milushev <milushev.sashko@gmail.com>
2023-03-06 12:21:50 +0200

    Add context for logged_in_user in home view

---
+++ hotel_management_software/common/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic as views
+
+
+class HomeView(views.TemplateView):
+    template_name = 'base/home-page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['logged_in_user'] = 'In' if self.request.user.is_authenticated else 'Not in'
+        return context
