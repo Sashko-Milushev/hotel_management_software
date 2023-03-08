bdb79e60765892037837b6484b72289107cdc784
Sashko Milushev <milushev.sashko@gmail.com>
2023-03-08 11:47:51 +0200

    Add Room model

--- hotel_management_software/rooms/models.py
+++ hotel_management_software/rooms/models.py
@@ -1,3 +1,35 @@
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    ROOM_MIN_CAPACITY = 1
+    ROOM_MAX_CAPACITY = 6
+
+    ROOM_MAX_LEN = 4
+    ROOM_MIN_LEN = 1
+
+    number = models.CharField(
+        max_length=ROOM_MAX_LEN,
+        validators=[validators.MinLengthValidator(ROOM_MIN_LEN)],
+        unique=True,
+        blank=False,
+        null=False
+
+    )
+
+    capacity = models.IntegerField(
+        validators=[validators.MinValueValidator(ROOM_MIN_CAPACITY),
+                    validators.MaxValueValidator(ROOM_MAX_CAPACITY), ],
+
+        blank=False,
+        null=False
+    )
+
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        editable=True,
+        blank=False,
+        null=False
+    )
